b773c7309d2599fcf4d835f24ade610f24fba7e6
YLIDKO <artyom_litvin@tut.by>
2021-06-02 15:10:11 +0300

    login page tests

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес/ что подстрока "login" есть в текущем url браузера. Для этого используйте соответствующее свойство Webdriver.
+        assert "login" in str(self.browser.current_url), "Should be login page"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "There is no login form"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "There is no registration form"
