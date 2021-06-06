fa8e832034ec9decd3fe5b6491a9d2b2e7350e7b
YLIDKO <artyom_litvin@tut.by>
2021-06-06 18:30:31 +0300

    product page locators was added

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,6 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, ".login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, ".register_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
