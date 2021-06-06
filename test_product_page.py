18b31ec9f07139f2bd9dafe6bae4c67690c17ce1
YLIDKO <artyom_litvin@tut.by>
2021-06-06 18:33:50 +0300

    product tests

---
+++ test_product_page.py
@@ -0,0 +1,17 @@
+from pages.product_page import ProductPage
+import time
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_cart_button()
+    page.add_product_to_cart()
+    page.should_be_success_message()
+    page.cost_of_product()
+
+
+
+
+
+
