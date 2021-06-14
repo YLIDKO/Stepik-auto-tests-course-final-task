b2e2a5094ee2b1938094946dea804b23cee82326
YLIDKO <artyom_litvin@tut.by>
2021-06-14 11:33:39 +0300

    is_elemet_present and is_disappeared

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,7 +1,6 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
 import pytest
-import time
 
 
 class ProductPage(BasePage):
@@ -18,14 +17,28 @@ class ProductPage(BasePage):
     def add_product_to_cart(self):
         add_to_cart_button = self.browser.find_element_by_css_selector(".btn-add-to-basket")
         add_to_cart_button.click()
-        self.solve_quiz_and_get_code()
+
+
+    def solve_quiz_and_get_code(self):
+        self.alert_quiz()
 
 
     def should_be_success_message(self):
         add_to_basket_message = self.browser.find_element_by_xpath("//div[@class='alertinner ']/strong").text
         assert add_to_basket_message == self.get_name_of_product(), "Wrong product was added"
 
+
     def cost_of_product(self):
         product_cost = self.browser.find_element_by_css_selector(".product_main p:nth-child(2)").text
         basket_cost = self.browser.find_element_by_css_selector(".basket-mini").text
         assert product_cost == basket_cost.split()[2], "The prices are not equal"
+
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+
+    def should_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
