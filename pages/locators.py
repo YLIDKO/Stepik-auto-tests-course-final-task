a55ae2e2a8c4a2d2d2bcfc782286128e8a2edc0c
YLIDKO <artyom_litvin@tut.by>
2021-06-14 11:31:26 +0300

    ProductPage: Success message selector. Added BasePageLocators for login

--- pages/locators.py
+++ pages/locators.py
@@ -9,5 +9,12 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, ".login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, ".register_form")
 
+
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
