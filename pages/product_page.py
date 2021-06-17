512751e8b700085af2585b2913a5312331fab558
YLIDKO <artyom_litvin@tut.by>
2021-06-17 13:52:31 +0300

    nothing happened

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,12 +6,11 @@ import pytest
 class ProductPage(BasePage):
 
     def should_be_add_to_cart_button(self):
-        assert self.is_element_present(*ProductPageLocators.BASKET_BUTTON), "There is no add to cat button"
+        assert self.is_element_present(*ProductPageLocators.BASKET_BUTTON), "There is no 'add to cart'   button"
 
     def get_name_of_product(self):
         product_name = self.browser.find_element_by_css_selector(".product_main h1")
         return product_name.text
-
 
 
     def add_product_to_cart(self):
