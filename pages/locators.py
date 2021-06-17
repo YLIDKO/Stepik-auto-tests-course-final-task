c5b1d46b6b9874620d297785ede3a940edb1d03b
YLIDKO <artyom_litvin@tut.by>
2021-06-17 13:48:35 +0300

    basket locators

--- pages/locators.py
+++ pages/locators.py
@@ -18,3 +18,9 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class BasketPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini.pull-right a")
+    IS_BASKET_EMPTY = (By.CSS_SELECTOR, ".basket-items")
+    EMPTY_BASKET_TEXT = (By.CSS_SELECTOR, "#content_inner p")
