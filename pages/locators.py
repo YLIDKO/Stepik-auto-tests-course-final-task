1893f4e87a1224980b3799bfaf71854275a7e805
YLIDKO <artyom_litvin@tut.by>
2021-06-22 13:43:48 +0300

    Registration form locators was added

--- pages/locators.py
+++ pages/locators.py
@@ -8,16 +8,22 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, ".login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, ".register_form")
+    REGISTRATION_FORM_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_FORM_PASSWORD_1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_FORM_PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTRATION_FORM_BUTTON = (By.CSS_SELECTOR, ".register_form .btn-primary")
 
 
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
 
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators():
