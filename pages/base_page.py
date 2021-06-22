3f0cda147e6f7f8f6c7e4d652f4449fb450f656d
YLIDKO <artyom_litvin@tut.by>
2021-06-22 13:43:09 +0300

    'is user autorised' check

--- pages/base_page.py
+++ pages/base_page.py
@@ -74,3 +74,6 @@ class BasePage():
         button = self.browser.find_element(*BasketPageLocators.BASKET_BUTTON)
         button.click()
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
