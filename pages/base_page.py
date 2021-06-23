f99259fca295c6fa1451cacbdeac069e622389d5
YLIDKO <artyom_litvin@tut.by>
2021-06-23 10:24:19 +0300

    refactoring

--- pages/base_page.py
+++ pages/base_page.py
@@ -9,13 +9,9 @@ import math
 
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        #self.browser.implicitly_wait(timeout)
-
-    def open(self):
-        self.browser.get(self.url)
 
 
     def alert_quiz(self):
@@ -32,6 +28,24 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
 
+
+    def go_to_basket(self):
+        button = self.browser.find_element(*BasketPageLocators.BASKET_BUTTON)
+        button.click()
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
 
 
     def is_element_present(self, how, what):
@@ -50,29 +64,13 @@ class BasePage():
 
         return False
 
-
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
-                until_not(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return False
-
-        return True
-
-
-    def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
-        link.click()
+    def open(self):
+        self.browser.get(self.url)
 
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
-
-    def go_to_basket(self):
-        button = self.browser.find_element(*BasketPageLocators.BASKET_BUTTON)
-        button.click()
 
     def should_be_authorized_user(self):
         assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
