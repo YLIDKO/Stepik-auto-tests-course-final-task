d7ace80c305c0866d466a1ddb1cd032364ce0120
YLIDKO <artyom_litvin@tut.by>
2021-06-23 12:54:55 +0300

    functions go to class methods

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
-from pages.main_page import MainPage
-from pages.login_page import LoginPage
-from pages.basket_page import BasketPage
 import pytest
+from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
+from pages.main_page import MainPage
 
 
 @pytest.mark.login_guest
@@ -15,13 +15,11 @@ class TestLoginFromMainPage():
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
-
     def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
-
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
