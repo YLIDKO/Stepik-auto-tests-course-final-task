4a1953cacd40fa432106c63265da121e987db03c
YLIDKO <artyom_litvin@tut.by>
2021-06-23 10:19:44 +0300

    refactoring

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,15 +9,12 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес/ что подстрока "login" есть в текущем url браузера. Для этого используйте соответствующее свойство Webdriver.
         assert "login" in str(self.browser.current_url), "Should be login page"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "There is no login form"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "There is no registration form"
 
     def register_new_user(self, email, password):
